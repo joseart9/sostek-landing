a05f7810ef00e8dde5dbf60e46af4c816dbe7c20
Arturo <A01177145@tec.mx>
2024-03-18 19:40:44 -0600

    People component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
 import Carousel from "@/components/Carousel";
+import PeopleCard from "@/components/PeopleCard";
+
+const people = [
+  {
+    name: "User",
+    role: "Role",
+  },
+  {
+    name: "User",
+    role: "Role",
+  },
+  {
+    name: "User",
+    role: "Role",
+  },
+  {
+    name: "User",
+    role: "Role",
+  },
+];
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <Carousel />
+      <section id="carousel">
+        <Carousel />
+      </section>
+      <section className="flex flex-row p-4 space-x-4" id="equipo">
+        {people.map((person, index) => (
+          <PeopleCard
+            key={index}
+            name={person.name}
+            role={person.role}
+            img="https://source.unsplash.com/random"
+          />
+        ))}
+      </section>
     </div>
   );
 }
