09f1e6b413409c0dac00a54e97ba611dc6a08172
YUDIPALACIOS <A00834231@tec.mx>
2024-04-06 19:56:47 -0600

    nosotros componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Carousel from "@/components/Carousel";
+import Nosotros from "@/components/Nosotros";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Carousel />
+      <Nosotros />
     </div>
+    
   );
 }
--- src/components/Nosotros.tsx
+++ src/components/Nosotros.tsx
@@ -1,7 +1,7 @@
 const Nosotros = () => {
   return (
     <div className="flex flex-col items-center">
-      <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" alt="Imagen de encabezado" className="w-full" />
+      <img src="https://www.google.com/url?sa=i&url=http%3A%2F%2Frecla.org%2Fblog%2Fsostenibilidad-social%2F&psig=AOvVaw3qsXxRMwN-u5vTfypyDTj6&ust=1712541094625000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCxg-v_roUDFQAAAAAdAAAAABAE" alt="Imagen de encabezado" className="w-full" />
       <div className="flex justify-center w-full py-4 gap-4">
         <div className="block">
           <h2 className="text-xl font-bold">Bloque 1</h2>
