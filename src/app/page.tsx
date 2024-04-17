48449ca91e88791dd7ce0162ba9aa6dc5d10e98e
Arturo <A01177145@tec.mx>
2024-04-17 14:57:30 -0600

    Updated People Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,20 @@ import Carousel from "@/components/Carousel";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import PeopleCard from "@/components/PeopleCard";
+import Nosotros from "@/components/Nosotros";
 
-const people = [
-  {
-    name: "User",
-    role: "Role",
-  },
-  {
-    name: "User",
-    role: "Role",
-  },
-  {
-    name: "User",
-    role: "Role",
-  },
-  {
-    name: "User",
-    role: "Role",
-  },
+const peopleData = [
+  { name: 'Martha Nuñez', role: 'Directora', img: 'imagen1.jpg', description: 'Directora de Carrera de Diseño Industrial, Tecnológico de Monterrey, campus Monterrey, México. Doctorado con Birmingham City University, Reino Unido. Directora de Carrera de Diseño Industrial, Tecnológico de Monterrey, campus Monterrey, México. Doctorado con Birmingham City University, Reino Unido. ' },
+  { name: 'Daniel Savedra', role: 'Director asociado', img: 'imagen2.jpg', description: 'Arquitecto con una vista del desarrollo de las ciudades desde una visión holística de sistemas complejos.' },
+  { name: 'Pedro Dario', role: 'Estudiante de Arquitectura', img: 'imagen3.jpg', description: 'En 2021 formo parte del equipo para la exposición del Tecnológico de Monterrey en la 17 º edición de la Bienal de Arquitectura de Venecia.' },
+  { name: 'Diana Montañez', role: 'Licenciada en Animación y Arte Digital', img: 'imagen4.jpg', description: 'Mis intereses son diseño editorial, diseño conceptual e innovación. ' },
+  { name: 'Aura Elena', role: 'Profesora', img: 'imagen5.jpg', description: 'Profesora interesada en colaborar con instituciones nacionales e internacionales en el fortalecimiento de acciones para reducir el cambio climático y lograr avances en mecanismos ambientales para un desarrollo sustentable.' },
+  { name: 'Eduardo Ferrer', role: 'Doctor en Ciencias de la Computación', img: 'imagen6.jpg', description: 'Mis intereses son el avance de tecnologías como simuladores, aplicaciones y videojuegos que mejoren el proceso de enseñanza-aprendizaje, adaptándose a los intereses y estilos de cada individuo.' },
+  { name: 'Santiago Penaloza', role: 'Ex colaborador', img: 'imagen7.jpg', description: 'Estudiante de LAD' },
+  { name: 'Fernando Fonseca', role: 'Ex colaborador', img: 'imagen8.jpg', description: 'Estudiante de LAD' },
+  { name: 'Sebastian Zafra', role: 'Ex colaborador', img: 'imagen9.jpg', description: 'Estudiante de LAD' },
+  { name: 'Marisol Sanchez', role: 'Ex colaborador', img: 'imagen10.jpg', description: 'Estudiante de IDS' },
+  { name: 'Alexis Sanchez', role: 'Ex colaborador', img: 'imagen10.jpg', description: 'Estudiante de LAD' },
 ];
 
 export default function Home() {
@@ -28,18 +24,14 @@ export default function Home() {
       <section id="carousel" className="w-full">
         <Carousel />
       </section>
+      <section>
+        <Nosotros />
+      </section>
       <h2 className="text-2xl mt-10 mb-5">
         Nuestro equipo
       </h2>
       <section className="flex flex-wrap justify-center gap-4 p-4" id="equipo">
-        {people.map((person, index) => (
-          <PeopleCard
-            key={index}
-            name={person.name}
-            role={person.role}
-            img="https://source.unsplash.com/random"
-          />
-        ))}
+        <PeopleCard people={peopleData} />
       </section>
       <section>
         <Contact />
--- src/components/Nosotros.tsx
+++ src/components/Nosotros.tsx
@@ -4,34 +4,30 @@ import './Nosotros.css';
 const Nosotros = () => {
   return (
     <div className="flex flex-col items-center">
-      <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" alt="Imagen de encabezado" className="w-full" />
       <div className="flex justify-center w-full py-4 gap-4">
         <div className="bloque">
           <h2 className="text-xl font-bold">Bloque 1</h2>
           <p>¿Cómo nace Sostek?</p>
           <p>Este proyecto surge de un interés por generar una cultura de sostenibilidad a través de la educación.
-             Una previa etapa de investigación, nos permitió detectar áreas de oportunidades en los planes educativos
-             de las diversas carreras involucradas.
-             Combinando esfuerzos de educadores y estudiantes generamos la primera versión de SOSTEK, una plataforma digital
-             que se alimentaría de los contenidos y requerimientos en temas de sostenibilidad, presentes en los proyectos
-              desarrollados en nuestra facultad.</p>
-          <textarea className="textarea" rows={4} cols={50}></textarea>
+            Una previa etapa de investigación, nos permitió detectar áreas de oportunidades en los planes educativos
+            de las diversas carreras involucradas.
+            Combinando esfuerzos de educadores y estudiantes generamos la primera versión de SOSTEK, una plataforma digital
+            que se alimentaría de los contenidos y requerimientos en temas de sostenibilidad, presentes en los proyectos
+            desarrollados en nuestra facultad.</p>
         </div>
         <div className="bloque">
           <h2 className="text-xl font-bold">Bloque 2</h2>
           <p>Iniciativa NOVUS</p>
           <p>NOVUS es una iniciativa del Tecnológico de Monterrey que apoya al desarrollo de ideas innovadoras a través
-             de proyectos prácticos y de investigación. El propósito de NOVUS es fomentar la participación estudiantil en
-             la implementación de medidas de mejora en el contenido educativo de sus respectivas facultades.  </p>
-          <textarea className="textarea" rows={4} cols={50}></textarea>
+            de proyectos prácticos y de investigación. El propósito de NOVUS es fomentar la participación estudiantil en
+            la implementación de medidas de mejora en el contenido educativo de sus respectivas facultades.  </p>
         </div>
         <div className="bloque">
           <h2 className="text-xl font-bold">Bloque 3</h2>
           <p>¿Quiénes somos?</p>
           <p>Somos un equipo de educadores y alumnos interesados en el desarrollo de estrategias encaminadas a una mayor
-             comprensión del alcance de los proyectos académicos como respuesta a las necesidades culturales, económicas
-             y ambientales de la sociedad. </p>
-          <textarea className="textarea" rows={4} cols={50}></textarea>
+            comprensión del alcance de los proyectos académicos como respuesta a las necesidades culturales, económicas
+            y ambientales de la sociedad. </p>
         </div>
       </div>
     </div>
